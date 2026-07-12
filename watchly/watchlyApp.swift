63839c82ed24d99df28f6a8ff8302882a619adef
ahmetaksoy10 <a.aksoy1020@gmail.com>
2026-07-12 19:16:52 +0300

    Initial Commit

---
+++ watchly/watchlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  watchlyApp.swift
+//  watchly
+//
+//  Created by MacBook Pro on 12.07.2026.
+//
+
+import SwiftUI
+
+@main
+struct watchlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
