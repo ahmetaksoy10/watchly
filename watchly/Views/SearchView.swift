d85625b9772d83c74f2605f07f6845e897b6ad9d
ahmetaksoy10 <a.aksoy1020@gmail.com>
2026-07-15 21:38:28 +0300

    feat: implement SearchView with dynamic movie filtering and navigation

--- watchly/Views/SearchView.swift
+++ watchly/Views/SearchView.swift
@@ -8,10 +8,39 @@
 import SwiftUI
 
 struct SearchView: View {
+    @State private var searchText = ""
+    let allMovies = MockData.sampleMovies
+
+    var filteredMovies: [Movie] {
+        if searchText.isEmpty {
+            return allMovies
+        }
+        return allMovies.filter {
+            $0.title.localizedCaseInsensitiveContains(searchText)
+        }
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            List(filteredMovies) { movie in
+                NavigationLink(destination: MovieDetailView(movie: movie)) {
+                    HStack{
+                        Text(movie.title)
+                            .font(.title3)
+                            .fontWeight(.semibold)
+                        
+                        Spacer()
+                        
+                        Text("⭐️ \(movie.voteAverage, specifier: "%.1f")")
+                    }
+                }
+            }
+            .navigationTitle("Arama")
+            .searchable(text: $searchText, prompt: "Film ara")
+        }
     }
 }
+
 
 #Preview {
     SearchView()
