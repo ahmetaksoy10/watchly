ae98cd80362df4b9d1bbb542b4f2e31e085fd6e1
ahmetaksoy10 <a.aksoy1020@gmail.com>
2026-07-24 21:22:06 +0300

    feat: add DiscoverViewModel to handle trending and popular movies

---
+++ watchly/ViewModels/DiscoverVİewModel.swift
@@ -0,0 +1,47 @@
+//
+//  DiscoverVİewModel.swift
+//  watchly
+//
+//  Created by MacBook Pro on 24.07.2026.
+//
+
+import Foundation
+import SwiftUI
+import Combine
+
+@MainActor
+final class DiscoverVİewModel: ObservableObject {
+    @Published var trendingMovies: [Movie] = []
+    @Published var popularMovies: [Movie] = []
+    @Published var isLoading: Bool = false
+    @Published var errorMessage: String? = nil
+    
+    init() {
+        Task{
+            await fetchMovies()
+        }
+    }
+    
+    func fetchMovies() async {
+        isLoading = true
+        errorMessage = nil
+        
+        defer {
+            isLoading = false
+        }
+        
+        do {
+            let fetchedTrending = try await TMDBService.shared.fetchTrendingMovies()
+            let fetchedPopular = try await TMDBService.shared.fetchPopularMovies()
+            
+            self.trendingMovies = fetchedTrending
+            self.popularMovies = fetchedPopular
+
+        } catch {
+            self.errorMessage = error.localizedDescription
+            print("Veri çekilirken hata oluştu: \(error.localizedDescription)")
+        }
+    }
+}
+
+
